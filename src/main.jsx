1aaa57dc8ebce588319ff067f10af08a3383b92c
Juan Pablo Gonzalez <juanpg@gmail.com>
2023-06-21 10:08:22 -0700

    Moved provider up the tree

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
+import { LegoArtProvider } from './Context/LegoArtContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <LegoArtProvider>
+        <App />
+      </LegoArtProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
